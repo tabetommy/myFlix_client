603a0da607986dce7ce22c747de96a1b04124f6f
Tommy Egbe <tabe_e@yahoo.ca>
2022-06-22 00:48:51 +0200

    added the movie view component

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import MovieCard from '../movie-card/movie-card';
+import MovieView from '../movie-view/movie-view';
+
  
 class MainView extends React.Component {
 
@@ -28,19 +30,29 @@ class MainView extends React.Component {
           Director:'Alan Taylor'
         }
       ],
+      seletedMovie:null
     }
   }
 
+  setSelectedMovie(newSelectedmovie){
+    this.setState({selectedMovie:newSelectedmovie})
+  }
+
   render() {
-    const {movies} = this.state;
+    const { movies, selectedMovie } = this.state;
+
+  if (selectedMovie) return <MovieView movie={selectedMovie} 
+    onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />;
+
   if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
 
   return (
     <div className="main-view">
-       {movies.map(movie => <MovieCard key={movie._id} movie={movie}/>)}
+      {movies.map(movie => <MovieCard key={movie._id} movie={movie} 
+        onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />)}
     </div>
   );
   }
 }
 
-export default MainView
+export default MainView;
