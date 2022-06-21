be2010521b0a89ce09589d0448f411b8bff2aa84
Tommy Egbe <tabe_e@yahoo.ca>
2022-06-22 00:06:05 +0200

    added the main view component

---
+++ components/MainView/main-view.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+ 
+class MainView extends React.Component {
+
+   constructor(){
+    super();
+    this.state = {
+      movies: [
+        {
+         _id: 1, 
+         Title: 'King of the boys', 
+        Description: 'Her insatiable appetite for power drives Eniola, a businesswoman and philanthropist, into politics. As she is drawn into a struggle for power her criminal baggage will prove a heavy burden',
+        ImagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbmWM7CfDhKzbV_vOjIj3Gjz-1Q774vRInMA&usqp=CAU',
+        Director:'Kemi Adetiba'
+        },
+        { 
+          _id: 2, 
+          Title: 'War room',
+          Description: 'The Jordan family looks ideal on the surface, yet has many cracks underneath. When Miss Clara observes this, she tries to guide them and help them unite again with the help of prayer.', 
+          ImagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbmWM7CfDhKzbV_vOjIj3Gjz-1Q774vRInMA&usqp=CAU'
+        },
+        { 
+          _id: 3, 
+          Title: 'Game of Thrones',
+          Description: 'Nine noble families wage war against each other in order to gain control over the mythical land of Westeros. Meanwhile, a force is rising after millenniums and threatens the existence of living men',
+          ImagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbRjeVSNUbGRT7yTB38U5_qNSbnSUfqnccjA&usqp=CAU',
+          Director:'Alan Taylor'
+        }
+      ],
+    }
+  }
+
+  render() {
+    const {movies} = this.state;
+  if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+  return (
+    <div className="main-view">
+      {movies.Title}
+    </div>
+  );
+  }
+}
+
+export default MainView
