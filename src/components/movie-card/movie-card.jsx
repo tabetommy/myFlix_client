a623412b886cf1e74cd4b2038bdb482bab6dc2dd
tabetommy <tabe_e@yahoo.ca>
2026-04-21 14:06:58 +0200

    rendered movie cards more interactive

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -9,39 +9,55 @@ import { Heart, HeartFill } from 'react-bootstrap-icons';
 
 
 
-function MovieCard(props)  {
- const { movie, userFavMovies, setUserFavMovies } = props;
- 
+function MovieCard(props) {
+	const { movie, userFavMovies, setUserFavMovies } = props;
+
 	function addMovie(id) {
 		let token = localStorage.getItem('token');
-		let username= localStorage.getItem('user');
-		const url= `https://movieapi-production-2da7.up.railway.app/users/${username}/movies/${id}`
-		axios.put(url,{},{
+		let username = localStorage.getItem('user');
+		const url = `https://movieapi-production-2da7.up.railway.app/users/${username}/movies/${id}`
+		axios.put(url, {}, {
 			headers: { Authorization: `Bearer ${token}` }
-		  })
-		  .then(response => {
-			// Update the userFavMovies state with the new list of favorite movies
-			setUserFavMovies(response.data.FavouritesMovies);
-		  })
-		  .catch(err => console.log(err))
-	  }
-
-	
-		return (
-			<Card className="movie-card">
-				<Card.Img className="movie-poster" src={movie.ImagePath} alt='movie-portrait' />
-				<Card.Body>
-					<Card.Title>{movie.Title}</Card.Title>
-					<Link to={`/myFlix_client/movies/${movie._id}`}>
-						<Button variant="secondary">Attributes</Button><br></br>
-					</Link>
-					<Button onClick={()=>addMovie(movie._id)} variant="light">
-						{userFavMovies?.includes(movie?._id) ? <HeartFill size={30} color="#585a5b" /> : <Heart size={30} color="#373738"/> }
-					</Button>
-				</Card.Body>
-			</Card>
-		)
+		})
+			.then(response => {
+				// Update the userFavMovies state with the new list of favorite movies
+				setUserFavMovies(response.data.FavouritesMovies);
+			})
+			.catch(err => console.log(err))
 	}
+
+
+	return (
+		<Card className="h-100 border-0 shadow-sm movie-card-custom">
+			<div className="position-relative overflow-hidden">
+				<Card.Img
+					variant="top"
+					src={movie.ImagePath}
+					className="movie-poster-img"
+				/>
+				{/* Floating Favorite Button */}
+				<button
+					onClick={() => addMovie(movie._id)}
+					className="fav-btn-overlay"
+				>
+					{userFavMovies?.includes(movie?._id)
+						? <HeartFill size={22} color="#ff4d4f" />
+						: <Heart size={22} color="#fff" />
+					}
+				</button>
+			</div>
+
+			<Card.Body className="d-flex flex-column justify-content-between p-3">
+				<Card.Title className="fs-6 fw-bold mb-3">{movie.Title}</Card.Title>
+				<Link to={`/myFlix_client/movies/${movie._id}`} className="w-100">
+					<Button variant="outline-primary" className="w-100 btn-sm rounded-pill">
+						View Details
+					</Button>
+				</Link>
+			</Card.Body>
+		</Card>
+	)
+}
 
 MovieCard.propTypes = {
 	movie: PropTypes.shape({
